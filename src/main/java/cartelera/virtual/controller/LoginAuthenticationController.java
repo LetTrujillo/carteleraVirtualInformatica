1d2c74c6cd8e230c88ebd3bf68c869a28ae04eba
Leticia Trujillo <leticia.trujillo@fluxit.com.ar>
2017-01-18 14:38:42 -0300

    Prueba token

--- src/main/java/cartelera/virtual/controller/LoginAuthenticationController.java
+++ src/main/java/cartelera/virtual/controller/LoginAuthenticationController.java
@@ -1,5 +1,11 @@
 package cartelera.virtual.controller;
 
+import java.net.URLEncoder;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,11 +20,16 @@ import cartelera.virtual.common.error.ResponseError;
 import cartelera.virtual.dto.AlumnoDTO;
 import cartelera.virtual.dto.LoginInfoDTO;
 import cartelera.virtual.exception.FindException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RequestMapping("/api/loginAuthentication")
 @Controller
 public class LoginAuthenticationController {
 	
+	private static final int expirationHours = 1;
+	private String secret = "mySecret";
 	@Autowired
 	private GuaraniBOImpl guaraniBO;
 
@@ -28,13 +39,26 @@ public class LoginAuthenticationController {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody ResponseEntity<?> authentication(@RequestBody LoginInfoDTO login) {
+	public @ResponseBody ResponseEntity<?> authentication(HttpServletRequest request, @RequestBody LoginInfoDTO login) {
 		try {
-			String token= "";
+			ResponseEntity<?> re = null;
+			String token = null;
 			//FIXME - recuperar un usuario genérico, luego validar el rol
 			AlumnoDTO loginUser = guaraniBO.alumnoLogin(login.getUsername(), login.getPassword());
 			if(loginUser != null && !"".equals(loginUser)){
 				//crear jwt-token
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(new Date());
+				Claims claims = Jwts.claims().setSubject(login.getUsername());
+				claims.put("username", login.getUsername());
+				//va la pass?
+				claims.put("password", login.getPassword());
+				claims.put("created", cal.getTime());
+				cal.add(Calendar.HOUR_OF_DAY, expirationHours);
+				claims.put("expiration", cal.getTime());
+				token = URLEncoder
+						.encode(Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS512, secret).compact());
+
 			}
 			return new ResponseEntity<String>(token, HttpStatus.OK);
 		} catch (FindException e) {
@@ -45,6 +69,17 @@ public class LoginAuthenticationController {
 		}
 		
 		
+	}
+	
+	
+	private Claims getClaimsFromToken(String token) {
+		Claims claims;
+		try {
+			claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			claims = null;
+		}
+		return claims;
 	}
 	
 }
